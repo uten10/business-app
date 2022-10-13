b898911d5bb9e0bcf5067eccf94fce25ca8a2dee
ambomu <uten@ambomus-MacBook-Air.local>
2022-10-13 17:59:44 -0400

    partners

--- asset.java
+++ asset.java
@@ -1,4 +1,3 @@
 Application will help me manage my business asset.
 This application logic will ensure all business assets are registered
-This new feature will help our business attribute special ids to all assets.
-Ensure all new assets are added to application inventory
+we worked on jenkins with flora on the phone
