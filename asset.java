8023958048c92277a6a46516601258ccf4493a80
ambomu <uten@ambomus-MacBook-Air.local>
2022-10-20 17:37:26 -0400

    mbong

--- asset.java
+++ asset.java
@@ -2,3 +2,4 @@ Application will help me manage my business asset.
 This application logic will ensure all business assets are registered
 rome-city at jenkins
 we on the second job at rome city
+jelisa on it again
